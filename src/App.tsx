7184542745933c4f1a458afa4ebc55ef1f551a2c
hlyamhtetkyaw <hlan1559@gmail.com>
2026-01-14 00:06:12 +0630

    [ADD] new transcribe

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BurmeseSummarizer from './component/BurmeseSummarizer'
+import VoiceInput from './component/Transcribe'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <BurmeseSummarizer/>
+      <VoiceInput/>
     </>
   )
 }
